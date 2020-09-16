022d2ae5cfbc9b3389cb16a1f4a8eabf2acf3d14
Rishabh <rishabhverma17@gmail.com>
2020-09-17 01:35:29 +0530

    Updated Tests

--- src/test/java/io/rishabh/MathUtilsTest.java
+++ src/test/java/io/rishabh/MathUtilsTest.java
@@ -1,14 +1,29 @@
 package io.rishabh;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
+
+    private MathUtils mathUtils;
+
+    @BeforeEach
+    void init(){
+        mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void cleanup(){
+        System.out.print("Cleaning...");
+    }
 
     @Test
     void testAdd() {
-        MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1,1);
         assertEquals(expected,actual, "The add method should add two numbers");
@@ -16,13 +31,11 @@ class MathUtilsTest {
 
     @Test
     void testComputeCircleArea() {
-        MathUtils mathUtils = new MathUtils();
         assertEquals(314.1592653589793,mathUtils.computeCircleArea(10),"Should return right circle area");
     }
 
     @Test
     void testDivide() {
-        MathUtils mathUtils = new MathUtils();
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), "Divide by zero should throw");
     }
 }
