35073ea18f097b11cb70e3447a4cb55eb3cee949
Rishabh <rishabhverma17@gmail.com>
2020-09-17 01:00:50 +0530

    First Commit

---
+++ src/test/java/io/rishabh/MathUtilsTest.java
@@ -0,0 +1,28 @@
+package io.rishabh;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void testAdd() {
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1,1);
+        assertEquals(expected,actual, "The add method should add two numbers");
+    }
+
+    @Test
+    void testComputeCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793,mathUtils.computeCircleArea(10),"Should return right circle area");
+    }
+
+    @Test
+    void testDivide() {
+        MathUtils mathUtils = new MathUtils();
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), "Divide by zero should throw");
+    }
+}
